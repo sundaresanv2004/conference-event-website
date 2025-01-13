29d4bdea17acab571253b6568751c69a5d355d68
Sundaresan V <sundaresanv2004@gmail.com>
2025-01-13 23:37:47 +0530

    UI Design Update

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,5 +1,7 @@
 import College_Logo from './college_logo.svg'
 import BackgroundImage from './bg1.png'
+import IEEE_Logo from './IEEE.png'
+import IEEECSL_Logo from './IEEECSL.png'
 import one_png from './1.jpeg'
 import two_png from './2.jpeg'
 import three_png from './3.jpg'
@@ -7,6 +9,8 @@ import three_png from './3.jpg'
 export {
     College_Logo,
     BackgroundImage,
+    IEEE_Logo,
+    IEEECSL_Logo,
     one_png,
     two_png,
     three_png,
